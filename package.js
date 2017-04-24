6ffdd6b8ee0c71b26eedb7b6add69c5235c2bc23
Nicholas Sardo <gcc.programmer@gmail.com>
2017-04-24 07:37:33 -0700

    needed to bump minor version for add of random seed, and optional param

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nsardo:random-password',
-  version: '1.0.0',
+  version: '1.2.0',
   summary: 'Creates a randomly generated password of selected length',
   git: 'https://github.com/nsardo/random-password.git',
   documentation: 'README.md'
